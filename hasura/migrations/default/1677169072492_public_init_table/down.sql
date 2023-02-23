686cfe8c4254f7d3012c9e37c00ca1bb0753d51d
Abraham Bekele <abrahambekele101@gmail.com>
2023-02-23 19:19:28 +0300

    brand new setup

---
+++ hasura/migrations/default/1677169072492_public_init_table/down.sql
@@ -0,0 +1,66 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- SET check_function_bodies = false;
+-- CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+--     LANGUAGE plpgsql
+--     AS $$
+-- DECLARE
+--   _new record;
+-- BEGIN
+--   _new := NEW;
+--   _new."updated_at" = NOW();
+--   RETURN _new;
+-- END;
+-- $$;
+-- CREATE TABLE public.movies (
+--     id integer NOT NULL,
+--     name text NOT NULL,
+--     overview text,
+--     poster_path text,
+--     release_date text,
+--     media_type text NOT NULL,
+--     created_at timestamp with time zone DEFAULT now() NOT NULL,
+--     updated_at timestamp with time zone DEFAULT now() NOT NULL
+-- );
+-- CREATE SEQUENCE public.movies_id_seq
+--     AS integer
+--     START WITH 1
+--     INCREMENT BY 1
+--     NO MINVALUE
+--     NO MAXVALUE
+--     CACHE 1;
+-- ALTER SEQUENCE public.movies_id_seq OWNED BY public.movies.id;
+-- CREATE TABLE public.users (
+--     id integer NOT NULL,
+--     first_name text NOT NULL,
+--     last_name text NOT NULL,
+--     email text NOT NULL,
+--     password text NOT NULL,
+--     "isVerified" boolean DEFAULT false NOT NULL,
+--     created_at timestamp with time zone DEFAULT now() NOT NULL,
+--     updated_at timestamp with time zone DEFAULT now() NOT NULL
+-- );
+-- CREATE SEQUENCE public.users_id_seq
+--     AS integer
+--     START WITH 1
+--     INCREMENT BY 1
+--     NO MINVALUE
+--     NO MAXVALUE
+--     CACHE 1;
+-- ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+-- ALTER TABLE ONLY public.movies ALTER COLUMN id SET DEFAULT nextval('public.movies_id_seq'::regclass);
+-- ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+-- ALTER TABLE ONLY public.movies
+--     ADD CONSTRAINT movies_name_key UNIQUE (name);
+-- ALTER TABLE ONLY public.movies
+--     ADD CONSTRAINT movies_overview_key UNIQUE (overview);
+-- ALTER TABLE ONLY public.movies
+--     ADD CONSTRAINT movies_pkey PRIMARY KEY (id);
+-- ALTER TABLE ONLY public.users
+--     ADD CONSTRAINT users_email_key UNIQUE (email);
+-- ALTER TABLE ONLY public.users
+--     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+-- CREATE TRIGGER set_public_movies_updated_at BEFORE UPDATE ON public.movies FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+-- COMMENT ON TRIGGER set_public_movies_updated_at ON public.movies IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+-- CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+-- COMMENT ON TRIGGER set_public_users_updated_at ON public.users IS 'trigger to set value of column "updated_at" to current timestamp on row update';
